f0ec24291453723369f7c912eda3de574e383b1a
Kuuragos <alejandrab.19401@gmail.com>
2019-11-25 18:04:58 -0500

    cambios

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -7,13 +7,39 @@ public class MovimientoEnemigo : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Camera camara;
+    public bool complete = false;
      void Awake()
     {
         camara = Camera.main;
     }
     void Start()
     {
-        agent.destination = GameObject.Find("Meta").transform.position;
+        complete = agent.hasPath;
+        int controlC = Random.Range(0, 3);
+        if (complete == false)
+        {
+            switch (controlC)
+            {
+                case 0:
+                    agent.destination = GameObject.Find("Top").transform.position;
+                    complete = true;
+                    break;
+                case 1:
+                    agent.destination = GameObject.Find("Mid").transform.position;
+                    complete = true;
+                    break;
+                case 2:
+                    agent.destination = GameObject.Find("Bot").transform.position;
+                    complete = true;
+                    break;
+            }
+        }
+        else if (complete == true)
+        {
+            agent.destination = GameObject.Find("Meta").transform.position;
+
+        }
+
     }
 
     void FixedUpdate()
@@ -23,5 +49,6 @@ public class MovimientoEnemigo : MonoBehaviour
         {
             agent.destination = hit.point;
         }
+        
     }
 }
