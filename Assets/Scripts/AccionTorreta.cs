ed09d4423fd83fd4796c30b8d0b23b42b429e416
Kuuragos <alejandrab.19401@gmail.com>
2019-12-03 15:02:53 -0500

    por si algo :V

--- Assets/Scripts/AccionTorreta.cs
+++ Assets/Scripts/AccionTorreta.cs
@@ -7,6 +7,8 @@ public class AccionTorreta : MonoBehaviour
     public Transform target, dirBalas;
     public GameObject prefabBala;
     public float rango = 10f;
+    public float fireRate = 1f;
+    public float fireCD;
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -20,23 +22,35 @@ public class AccionTorreta : MonoBehaviour
         foreach (GameObject enemy in enemigosEscena)
         {
             float rotateDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
             if (rotateDistance < identifyEnemy)
             {
                 identifyEnemy = rotateDistance;
                 nearestEnemy = enemy;
             }
         }
+
         if (nearestEnemy != null && identifyEnemy <= rango)
         {
             target = nearestEnemy.transform;
+
         }
         else
+        {
             target = null;
+
+        }
     }
     void Update()
     {
-        if (target = null)
+        if (target == null)
             return;
+        if (fireCD <= 0)
+        {
+            Shoot();
+            fireCD = 1f / fireRate;
+        }
+        fireCD -= Time.deltaTime;
     }
     private void OnDrawGizmosSelected()
     {
@@ -45,7 +59,12 @@ public class AccionTorreta : MonoBehaviour
     }
     void Shoot()
     {
-        Instantiate(prefabBala);
+        GameObject dirBala = (GameObject) Instantiate(prefabBala, dirBalas.position, dirBalas.rotation);
+        Balas bullet = dirBala.GetComponent<Balas>();
+        if (bullet != null)
+        {
+            bullet.Skeek(target);
+        }
     }
     private void FixedUpdate()
     {
---
+++ Assets/Scripts/Balas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balas : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 70f;
+    public void Skeek(Transform targetB)
+    {
+        target = targetB;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 magni = target.position - transform.position;
+        float disThisFrame = speed * Time.deltaTime;
+
+        if (magni.magnitude <= disThisFrame)
+        {
+            GolpeTarget();
+            return;
+        }
+    }
+
+    void GolpeTarget()
+    {
+        Destroy(gameObject);
+    }
+}
